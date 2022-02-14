25db71979b9a9ceb2047b4ecedc04f7059631b14
Lukasz Kalnik <lukasz.kalnik@grandcentrix.net>
2022-02-14 17:08:41 +0100

    Navigate with routes

--- app/src/main/java/de/dojo/weather/WeatherNavHost.kt
+++ app/src/main/java/de/dojo/weather/WeatherNavHost.kt
@@ -4,38 +4,46 @@ import androidx.compose.runtime.*
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import de.dojo.weather.ui.SettingsScreen.SettingsScreen
 import de.dojo.weather.ui.WeatherDetailScreen.WeatherDetailScreen
 import de.dojo.weather.ui.WeatherScreen.WeatherScreen
 
+private const val locationArgumentPlaceholder = "location"
+
 @Composable
 fun WeatherNavHost(
     navController: NavHostController,
-    startDestination: String = WeatherDestination.WEATHER.name,
 ) {
     var location: String by remember {
         mutableStateOf("headquarter")
     }
+    val startDestination: String = WeatherDestination.Weather(location = location).getLink()
 
     NavHost(navController, startDestination) {
-        composable(WeatherDestination.WEATHER.name) {
-            WeatherScreen(currentLocation = location,
+        composable(
+            route = WeatherDestination.Weather(location = location).getRoute(),
+            arguments = listOf(navArgument(locationArgumentPlaceholder) {})
+        ) {
+            WeatherScreen(
+                currentLocation = location,
                 onSettingsClick = {
-                    navController.navigate(WeatherDestination.SETTINGS.name)
-                }, onDetailClick = {
-                    navController.navigate(WeatherDestination.WEATHER_DETAIL.name)
+                    navController.navigate(WeatherDestination.Settings(location = location).getLink())
+                },
+                onDetailClick = {
+                    navController.navigate(WeatherDestination.Weather(location = location).getLink())
                 }
             )
         }
-        composable(WeatherDestination.SETTINGS.name) {
+        composable(WeatherDestination.Settings(location = location).getLink()) {
             SettingsScreen(
                 currentLocation = location,
                 onApplySettings = { chosenLocation ->
-                location = chosenLocation
-                navController.navigateUp()
-            })
+                    location = chosenLocation
+                    navController.navigateUp()
+                })
         }
-        composable(WeatherDestination.WEATHER_DETAIL.name) {
+        composable(WeatherDestination.WeatherDetail(location = location).getLink()) {
             WeatherDetailScreen(
                 location = "headquarter",
                 onBackClick = { navController.navigateUp() }
@@ -45,8 +53,21 @@ fun WeatherNavHost(
     }
 }
 
-private enum class WeatherDestination {
-    WEATHER,
-    WEATHER_DETAIL,
-    SETTINGS
+private sealed class WeatherDestination(
+    val destination: String,
+    open val location: String,
+) {
+
+    fun getRoute(): String = "$destination?location={$locationArgumentPlaceholder}"
+
+    fun getLink(): String = "$destination?location=$location"
+
+    data class Weather(override val location: String) :
+        WeatherDestination(destination = "WEATHER", location = location)
+
+    data class WeatherDetail(override val location: String) :
+        WeatherDestination(destination = "WEATHER_DETAIL", location = location)
+
+    data class Settings(override val location: String) :
+        WeatherDestination(destination = "SETTINGS", location = location)
 }
